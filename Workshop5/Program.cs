9fe1e1c4b902ab510646d0edc33ad6916722fa1d
Sheetalkatwal <sheetalkatwal97@gmail.com>
2025-11-26 11:28:41 +0545

    Task2 completed.

--- Workshop5/Program.cs
+++ Workshop5/Program.cs
@@ -22,5 +22,41 @@
 //    }
 //}
 
+//Task2
+
+using System;
+using System.Runtime.ConstrainedExecution;
+
+namespace Workshop5
+{
+    class Program
+    {
+        static void Main()
+        {
+            Car car = new Car();
+            car.Brand = "Toyota";
+            car.Speed = 120;
+            car.Seats = 4;
+
+            Motorcycle moto = new Motorcycle();
+            moto.Brand = "Yamaha";
+            moto.Speed = 90;
+            moto.Type = "Sport";
+
+            Console.WriteLine("Car Info:");
+            car.Start();
+            car.DisplayInfo();
+            car.Stop();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Motorcycle Info:");
+            moto.Start();
+            moto.DisplayInfo();
+            moto.Stop();
+        }
+    }
+}
+
 
 
---
+++ Workshop5/Task2.cs
@@ -0,0 +1,46 @@
+﻿namespace Workshop5
+{
+    class Vehicle
+    {
+        public string Brand { get; set; }
+        public int Speed { get; set; }
+
+        public void Start()
+        {
+            Console.WriteLine(Brand + " started.");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine(Brand + " stopped.");
+        }
+
+        public virtual void DisplayInfo()
+        {
+            Console.WriteLine("Brand: " + Brand);
+            Console.WriteLine("Speed: " + Speed);
+        }
+    }
+
+    class Car : Vehicle
+    {
+        public int Seats { get; set; }
+
+        public override void DisplayInfo()
+        {
+            base.DisplayInfo();
+            Console.WriteLine("Seats: " + Seats);
+        }
+    }
+
+    class Motorcycle : Vehicle
+    {
+        public string Type { get; set; }
+
+        public override void DisplayInfo()
+        {
+            base.DisplayInfo();
+            Console.WriteLine("Type: " + Type);
+        }
+    }
+}
