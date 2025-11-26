6388354d2df5588a02ea6c9ec7472c0aa8f1a2ae
Sheetalkatwal <sheetalkatwal97@gmail.com>
2025-11-26 11:26:43 +0545

    Add project files.

---
+++ Workshop5/Program.cs
@@ -0,0 +1,26 @@
+﻿
+////Task1
+//using System;
+
+//namespace Workshop5
+//{
+//    class Program
+//    {
+//        static void Main()
+//        {
+//            BankAccount acc = new BankAccount("12345", 500);
+
+//            Console.WriteLine("Account Number: " + acc.AccountNumber);
+//            Console.WriteLine("Initial Balance: " + acc.Balance);
+
+//            acc.Deposit(300);
+//            Console.WriteLine("After Deposit: " + acc.Balance);
+
+//            acc.Withdraw(200);
+//            Console.WriteLine("After Withdraw: " + acc.Balance);
+//        }
+//    }
+//}
+
+
+
---
+++ Workshop5/Task1.cs
@@ -0,0 +1,59 @@
+﻿//namespace Workshop5
+//{
+//    class BankAccount
+//    {
+//        private string accountNumber;
+//        private double balance;
+
+//        public string AccountNumber
+//        {
+//            get { return accountNumber; }
+//        }
+
+//        public double Balance
+//        {
+//            get { return balance; }
+//            private set
+//            {
+//                if (value > 0)
+//                {
+//                    balance = value;
+//                }
+//            }
+//        }
+
+//        public BankAccount(string accNum, double initialBalance)
+//        {
+//            accountNumber = accNum;
+//            if (initialBalance > 0)
+//            {
+//                balance = initialBalance;
+//            }
+//        }
+
+//        public void Deposit(double amount)
+//        {
+//            if (amount > 0)
+//            {
+//                balance += amount;
+//            }
+//            else
+//            {
+//                Console.WriteLine("Invalid deposit amount.");
+//            }
+//        }
+
+//        public void Withdraw(double amount)
+//        {
+//            if (amount > 0 && amount <= balance)
+//            {
+//                balance -= amount;
+//            }
+//            else
+//            {
+//                Console.WriteLine("Invalid withdraw amount.");
+//            }
+//        }
+//    }
+//}
+
